45c325e9b5f79cfcf41fa6db5735d12cbd1e936f
RGoralewski <radekgoralewski@gmail.com>
2020-01-10 00:18:42 +0100

    Display set matches in new window
    Changes:
    - Set matches! button that calls setMatches() function
    and create a new window (matchWindow)
    - In matchWindow set matches are displayed
    - In mainWindow textbox is cleared when player is added

--- chessGUI.py
+++ chessGUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import chessSetter
 
 #Window
 mainWindow = Tk()
@@ -26,6 +27,7 @@ def addPlayerClicked():
     players.append(txt.get())
     pLabels.append(Label(mainWindow, text=players[-1], font=("Calibri", 10)))
     pLabels[-1].grid(column=1, row=len(players))
+    txt.delete(0, END)
 addPlayerButton = Button(mainWindow, text="Add player", command=addPlayerClicked)
 addPlayerButton.grid(column=2, row=0)
 
@@ -38,6 +40,31 @@ def deletePlayerClicked():
         pLabels.pop()
 deletePlayerButton = Button(mainWindow, text="Delete", command=deletePlayerClicked)
 deletePlayerButton.grid(column=2, row=1)
+
+#Set matches button
+def setMachesClicked():
+    if len(players) > 1:
+        #Create a new window for set matches
+        matchesWindow = Tk()
+        matchesWindow.title("Matches")
+        matchesWindow.geometry('800x600')
+        #Set matches
+        matches = chessSetter.setMatches(players)
+        #Display matches
+        rowNum = 0
+        for m in matches:
+            lbl = Label(matchesWindow, text=str(rowNum + 1) + ". ")
+            lbl.grid(column=0, row=rowNum)
+            lbl = Label(matchesWindow, text=m[0])
+            lbl.grid(column=1, row=rowNum)
+            lbl = Label(matchesWindow, text=" - ")
+            lbl.grid(column=2, row=rowNum)
+            lbl = Label(matchesWindow, text=m[1])
+            lbl.grid(column=3, row=rowNum)
+            rowNum += 1
+
+setMatchesButton = Button(mainWindow, text="Set matches!", command=setMachesClicked, bg="orange")
+setMatchesButton.grid(column=3, row=0)
 
 #Mainloop
 mainWindow.mainloop()
